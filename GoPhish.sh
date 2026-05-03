fee169f241369c3b66fca2342f0d0527b9c5932c
hajaralsahaib <hajaralsahaib@gmail.com>
2026-05-03 22:13:56 +0300

    Add files via upload

---
+++ GoPhish.sh
@@ -0,0 +1,9 @@
+wget https://github.com/gophish/gophish/releases/latest/download/gophish-v0.12.1-linux-64bit.zip
+
+unzip gophish-v0.12.1-linux-64bit.zip~
+
+chmod +x gophish
+
+cd gophish
+
+sudo ./gophish
